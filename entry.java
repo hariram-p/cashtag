d6ede0b70f30ca69f15425e5fee5b20baf49b588
periyasamy <periguruaravi@gmai.com>
2021-08-13 15:07:14 +0530

    creating classes entry and abstracting

---
+++ entry.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class entry
+{
+	public int balance=0;
+	public FileReader capturefile(String file)
+	{
+		return new FileReader(file);
+	}
+	public static void main(String args[])
+	{
+	}
+}
