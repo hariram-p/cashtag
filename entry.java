9572d46a4a278aa1c39dbc29e9e1530abba51721
periyasamy <periguruaravi@gmai.com>
2021-08-14 15:00:00 +0530

    adding codes to entries file and structing entries

--- entry.java
+++ entry.java
@@ -2,11 +2,28 @@ import java.io.*;
 public class entry
 {
 	public int balance=0;
-	public FileReader capturefile(String file)
+	public FileReader reader;
+	public FileWriter writer;
+	public String entries="";
+	boolean havefile=true;
+	public entry(String file)
 	{
-		return new FileReader(file);
+		try{
+		reader =new FileReader(file);
+		}
+		catch(Exception E)
+		{
+			System.out.println("file not found");
+			havefile=false;
+		}
+		if(!havefile)
+		{
+			try{}
+		
+		}
 	}
 	public static void main(String args[])
 	{
+		new entry("entry.ct");
 	}
 }
